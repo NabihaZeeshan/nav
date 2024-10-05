d1dfb681f32103ca2c5e141a4656e93902fcda81
NabihaZeeshan <zeeshannabiha88@gmail.com>
2024-10-05 11:23:27 +0500

    Add files via upload

---
+++ java chapters/index.js
@@ -0,0 +1,395 @@
+// var societies = ["buchvillas", "sunshine" ,"sahar villas", "green town", "royal grace"]
+// constsole.log(societies);
+// societies.pop()
+// console.log(societies);
+// societies.push("lacknow", "bahria town")
+// console.log(societies);
+// societies.shift()
+// console.log(societies);
+// societies.unshift("gulgusht", "bosan road")
+// console.log(societies);
+
+
+// // forloops
+
+// for (i=1; i<=1; i++){
+//     console.log(`2 * ${i} = ${i*2}`);
+// }
+// for (i=2; i<=2; i++){
+//     console.log(`3 * ${i} = ${i*3}`);
+// }
+// for (i=3; i<=3; i++){
+//     console.log(`4 * ${i} = ${i*4}`);
+// }
+// for (i=4; i<=4; i++){
+//     console.log(`5 * ${i} = ${i*5}`);
+// }
+// for (i=5; i<=5; i++){
+//     console.log(`6 * ${i} = ${i*6}`);
+// }
+// for (i=6; i<=6; i++){
+//     console.log(`7 * ${i} = ${i*7}`);
+// }
+// for (i=7; i<=7; i++){
+//     console.log(`8 * ${i} = ${i*8}`);
+// }
+// for (i=8; i<=8; i++){
+//     console.log(`9 * ${i} = ${i*9}`);
+// }
+// for (i=9; i<=9; i++){
+//     console.log(`10 * ${i} = ${i*10}`);
+// }
+// for (i=10; i<=10; i++){
+//     console.log(`11 *${i}  = ${i*11}`);
+// }
+
+
+// for (let i=1; i<=100; i++){
+// console.log(i);
+// }
+
+// for (let i=1; i<=100;i++)
+//     if(i%2==0){
+//         console.log(i+ "is even");
+//     }
+//     else{
+//     console.log(i+ "is odd");
+//     }
+
+// ques no 1
+// alert("nabiha zeeshan");
+
+// // ques no 2
+// var myname = "nabiha";
+// var message  ="i am very excieted for web development course";
+
+// alert(`hello, my name is ${myname}\nand i just want to tell all of you that ${message}`);
+// console.log(`hello, my name is ${myname}\nand i just want totell all of you that ${message}`);
+
+// // ques no 3
+// var a=4
+// var b=2
+// var result = (a++ + --b - --a + b++ + a +b )
+
+// console.log(result)
+
+// // ques no 4
+// var enroll - prompt("listen all students tomorrow held xtra classes due to vacations lose?");
+// var secques - prompt("all students must have books & copies?");
+// var thirdques - prompt("and all students read complt course or have any question all questions ask me tomorrow class?");
+// if (enroll ==="ok miss" && secques ==="ok miss"&& thirdques === "ok miss"){
+
+// }
+
+// for (var i=1; i<=10; i++){
+    // console.log(`2* ${i} =${i*2}`);
+// }
+
+// for (var i=20; i>=1; i--){
+    // console.log("back counting:"+i);
+// }
+
+// ALERT
+
+// alert("hello world");
+
+// var age=[18, 20, 22, 24,34,44,55];
+// var newarray = []
+// for (var i=o; i<= ages.length; i++){
+//     if (ages[i] >18){
+//         newarray.push(ages[i]);
+//     }
+// }
+// console.log(newarray)
+
+// alert
+
+// var message=("hello world");
+// alert (message);
+
+
+// var message=(4+9);
+// alert(message);
+
+// var message=("4"+"9");
+// alert(message);
+
+// var message="welcome to the karachi";
+// document.write(message);
+// function
+// function info(){
+    // var question = prompt("what is your name?");
+    // var question = prompt("how old are you?");
+    // var question = orompt("in which class do you read?");
+    
+    // console.log(`my name is ${question},i am ${question}years old,in class${question}`)
+    
+    // if(question2<=10){
+        // console.log= `you are allowed in this class`
+    // }
+    // else{
+        // console.log= `you are not allowed in this class`
+    // }
+    // }
+    // info()
+    // console.log(`my name is ${quetion},i am  ${question} years old, in class ${question}`);
+
+
+// ALERT chapter
+
+// question no 1
+
+var answer=("alert show the output in front pop up User");
+alert (answer);
+
+// question no 2
+ var answer=("welcome to my website");
+ alert (answer);
+
+//  question no 3
+var answer=("if ALERT capitalize can't give the answer due to java is case sensitive language");
+alert (answer);
+
+// question no 4
+var syntax=("hello, world");
+alert (syntax);
+
+// question no 5
+// var format=("");
+ 
+// format = it is pop up  box show the message
+// { it is openinng paranthesis
+// }it is closing paranthesis
+// ; colon show the ending  line
+// alert (format);
+
+
+// functions
+function spagity(noodle,onion,tomato,cabbage,soyaSauce,carrot){
+
+var stationary = noodle + onion + tomato + cabbage + soyaSauce + carrot
+console.log(stationary)
+}
+spagity(1,3,3,1,1,1 )
+
+
+// function syntax
+
+// function functionName (){
+
+// }
+
+// variable for Strings
+// question no 1
+
+var message = "hello, world!";
+alert(message);
+
+// question no 2
+var age:
+age = 25;
+alert(age);
+
+// question n0 3
+var greeting;
+greeting = "good morning"
+alert(greeting);
+
+greeting = "good evening"
+alert (greeting);
+
+// question no 4
+var favoritecolor = "blue";
+alert("my favorite color is "+ favoritecolor);
+
+// variable for numbers
+// question no 1
+var height = 170;
+var newheight = height + 10;
+alert (newheight);
+
+// question no 2
+var num1 = 50;
+var num2 = 20;
+var sum = num1+num2;
+alert(sum);
+
+// question no 3
+// var num = 30;
+Value
+var total = num +20;
+alert(total);
+
+// question no 4
+var price = 120;
+var totalprice = price + (price*0.10);
+alert(totalprice);
+
+// question no 5
+var myaddress = "karachi";
+replace "lahore"
+alert(myaddress);
+
+// question no 6
+var username = "nabiha";
+var useremail = "nabiha@.com";
+var userpassword = "security123"
+
+// question no 7
+alert("username:" + userName +"email:" + userEmail +"userpassword:" +userpassword);
+var user address = "123 mainst";
+var userAddress = "123mainst";
+alert(userAddress);
+
+// question no 8
+var rose = "flurrybundle";
+alert(rose);
+alert(Rose);
+
+// math expression familier opertor
+// question n0 1
+var sum =25+ 30;
+alert (sum);
+
+// question no 2
+var num1 = 25;
+var num2 = 5;
+var sumdisplay = num1 + num2;
+alert(sumdisplay);
+
+// question no 3
+var modulusresult = 27%4;
+alert(modulusresult);
+
+// math expression unfamilier operator
+// question no 1
+var num =3;
+var newNum = num++;
+alert("num:" +num);
+alert("newNum:" + newNum);
+
+// QUESTION NO 2
+var counter = 10;
+counter += 5;
+counter -= 2;
+alert(counter);
+
+// question no 3
+var points = 20;
+var postdecrement = points--;
+var preincrement = ++points;
+alert(points);
+
+// question no 4
+var number = 0;
+number++;
+number++;
+alert(number);
+
+// math expression eliminating umbegity
+// question no 1
+var calculation = (20 % 3) + 5;
+alert(calculation);
+
+// question no 2
+var expressionValue = 6 + 2*5-3;
+alert(expressionValue);
+
+// questionno 3
+var finaltotal = 8+4*2-5/5;
+
+// question no 4
+var username = prompt("what is your name?");
+alert("hello," +username + "!");
+
+// prompts
+// question no 1
+var age = prompt("how old are you?");
+if (age === null){
+    alert("no help provided");
+} else{
+    alert ("you are "+ age +"years old!");
+}
+
+// question NO 2
+var pet = prompt("how many pets do you have?");
+if (pet === ""){
+    alert("you didn't enter anything.");
+}
+else {
+    alert("you have "+ pet +"pet(s).");
+}
+
+// question no 3
+var userInput = prompt("please enter a number between 1 and 10:");
+var number = number(userInput);
+if (!isNaN(Number)&& NUMBER >=1 && NUMBER <= 10){
+    var result = number*2;
+    alert("the result is: "+result);
+}
+else{
+    alert("plese enter a valid number 1 & 10.");
+}
+
+// question no 4
+var favoriteNum = prompt("what is your favorite number?");
+if (favoriteNum ===""){
+    favoriteNum = 7;
+}
+alert ("your favorite number is:" + favoriteNum);
+
+// question no 5
+let username = prompt("what is your name?");
+alert("hello" + username +"!");
+
+// if statment
+// question no 1
+let answer = prompt("what is the capital of france?");
+if (answer === "paris"){
+    alert("that is correct!. the capital of france is paris.");
+}
+else{
+    alert("that is correct!. the capital of france is paris.");
+}
+
+// question no 2
+let answer = prompt("what is te capital of france?");
+if (answer === "paris"){
+    alert("correct!");
+}
+else{
+    alert("incorrect. the correct answer is paris.");
+}
+
+// question no 3
+let answer = prompt("what is the capital of the united kingdom?");
+if (answer === "london" || answer === "united kingdom"){
+    alert("correct!");
+}
+else {
+    alert("incorrect. the correct answer is london.");
+}
+
+// question n0 4
+let score = 0
+function askQuestion(question, correctAnswer){
+    let answer = prompt(question);
+    if(answer === correctAnswer.tolowercase()){
+        alert("correct. the correct answer is " +correctAnswer +"your score remains:" +score);
+    }
+}
+askQuestion("what is the capital of france?" , "paris");
+askQuestion("what is the capital of united kingdom?","london");
+askQuestion("what is the capital of japan?", "tokyo");
+
+// question no 5
+let capitalofGermany = prompt("what is the capital of germany?");
+let mathAnswer = prompt("what is 10 + 10?");
+let iscapitalcorrect =  (capitalofgermany  ===  "berlin");
+let ismathcorrect = (mathAnswer ==="20");
+if (iscapitalcorrect && ismathcorrect){
+    alert("you got one correct.");
+}
+else{
+    alert("you got both wrong.");
+}
---
+++ java chapters/practice.js
@@ -0,0 +1,10 @@
+var societies = ["buchvillas", "sunshine" ,"sahar villas", "green town", "royal grace"]
+constsole.log(societies);
+societies.pop()
+console.log(societies);
+societies.push("lacknow", "bahria town")
+console.log(societies);
+societies.shift()
+console.log(societies);
+societies.unshift("gulgusht", "bosan road")
+console.log(societies);
